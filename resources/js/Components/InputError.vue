c4473241e57eb290cca887f56b4553ce0749920e
Danil Zaytsev <zaytsevdanil@yandex.ru>
2024-01-12 01:31:22 +0300

    dix

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,15 +1,15 @@
 <template>
-    <div v-show="message">
-        <p class="text-sm text-red-600 dark:text-red-400">
-            {{ message }}
-        </p>
-    </div>
+  <div v-show="message">
+    <p class="text-sm text-red-600 dark:text-red-400">
+      {{ message }}
+    </p>
+  </div>
 </template>
 
 <script setup>
 defineProps({
-    message: {
-        type: String
-    }
+  message: {
+    type: String
+  }
 });
 </script>
